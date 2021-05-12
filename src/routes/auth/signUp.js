767e5a7e7b2dd572e0ac579a9fca86fd3dfc93b3
alexbujenita <dekc89@gmail.com>
2021-05-12 18:17:09 +0100

    HER signup

--- src/routes/auth/signUp.js
+++ src/routes/auth/signUp.js
@@ -7,7 +7,9 @@ signUpRouter.post("/", async (req, res) => {
   const {
     body: { firstName, lastName, email, password },
   } = req;
+  console.log("SIGNUP ", {firstName, lastName, email, password})
   const hashedPassword = await bcrypt.hash(password, saltRounds);
+  console.log({hashedPassword})
   try {
     await db.User.create({
       firstName,
@@ -17,6 +19,7 @@ signUpRouter.post("/", async (req, res) => {
     });
     res.status(201).send({ message: "User succesfully created" });
   } catch (e) {
+    console.log(e)
     res
       .status(500)
       .send({ error: e?.errors[0]?.message ?? "Error creating user" });
