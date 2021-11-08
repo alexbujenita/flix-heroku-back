71a84bbd816205acc61e9afe3a1603d6ba3b5496
alexbujenita <dekc89@gmail.com>
2021-11-08 21:58:10 +0000

    correct import

--- src/routes/certifications/index.js
+++ src/routes/certifications/index.js
@@ -1,6 +1,6 @@
 const certificationsRouter = require("express").Router();
 const axios = require("axios");
-const API_KEY = require("../../../secrets").API_KEY;
+const API_KEY = process.env.TMDB_API;
 
 certificationsRouter.get("/", async (_, res) => {
   try {
