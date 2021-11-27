f9396399d7a8f388d8786caff7a9431faa749542
alexbujenita <dekc89@gmail.com>
2021-11-27 20:18:25 +0000

    correct admin :sigh:

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -6,8 +6,7 @@ const authJWT = require("../../middleware/auth/authJWT");
 const USER_ATTRB = ["id", "firstName", "lastName"];
 
 adminRouter.get("/users", authJWT, async (req, res) => {
-  console.log({loggedUser: req.loggedUser})
-  if (req.loggedUser !== 1) {
+  if (req.loggedUser !== 2) { // yeah yeah, shitty
     return res.sendStatus(403);
   }
   try {
