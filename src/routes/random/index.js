6f518aa0eecaa6eac8dacecb16d243770d091d11
alexbujenita <dekc89@gmail.com>
2022-08-19 17:42:00 +0100

    more random, mostly for new heroku stack

--- src/routes/random/index.js
+++ src/routes/random/index.js
@@ -10,16 +10,16 @@ randomRouter.get("/", async (_, res) => {
     let failSafe = 0;
 
     do {
-      if (++failSafe > 10) break; // return what we have instead of erroring if there are too many requests
+      if (++failSafe > 15) break; // return what we have instead of erroring if there are too many requests
       const rndQuery = randomQueryString();
       const { data } = await axios.get(rndQuery);
-      const iterations = randomInt(2, 7);
+      const iterations = randomInt(3, 7);
       for (let i = 0; i < iterations; i++) {
         const [movie, idx] = sample(data.results);
         randomMovies.push(movie);
         data.results.splice(idx, 1);
       }
-    } while (randomMovies.length < 27);
+    } while (randomMovies.length < 47);
 
     res.send(randomMovies);
   } catch (error) {
